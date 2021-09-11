0e6b9a78e8cd3f8aee3c41780ec8740ed64d61b0
Ryan Packer <ryan.packer@gmail.com>
2021-09-11 17:42:28 -0600

    cleanup TestCommand
    removing some of the test work that is now obviated by usage of ella7/console-command

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -6,16 +6,11 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class TestCommand extends InteractiveOptionCommand
 {
   protected static $defaultName = 'gpstools:test';
-
-  public function __construct()
-  {
-      //
-      parent::__construct();
-  }
 
   protected function configure()
   {
@@ -29,64 +24,34 @@ class TestCommand extends InteractiveOptionCommand
         'sub-command to run',
         false
       )
-      ->addOption(
-        'intopt',
-        null,
-        InputOption::VALUE_OPTIONAL,
-        'just testing here',
-        false
-      )
     ;
 
-    $question = new Question('This is the interactive question: ' . "\n> ");
-    $this->addInteractivityForOption('intopt', self::INTERACTION_ALWAYS, $question);
-
-  }
-
-  protected function interact(InputInterface $input, OutputInterface $output)
-  {
-    parent::interact($input, $output);
-    $helper = $this->getHelper('question');
-    $cmd = $input->getOption('cmd');
-
-    if(!$this->isValidSubCommand($cmd)){
-      if($cmd){
-        $this->cmdInputValidator($cmd);
-      }
-      $question = new Question('Which test command do you want to run?' . "\n> " , $this->validSubCommands()[0]);
-      $question->setValidator(function ($answer) {
-        return $this->cmdInputValidator($answer);
-      });
-      $input->setOption(
-        'cmd',
-        $helper->ask($input, $output, $question)
-      );
-    }
-  }
-
-  protected function cmdInputValidator($cmd)
-  {
-    if(!$this->isValidSubCommand($cmd)) {
-      throw new \RuntimeException(
-        '\'' . $cmd . '\' is not in the list of valid commands.'
-      );
-    }
-    return $cmd;
+    $question = new ChoiceQuestion('Which test command do you want to run?', $this->validSubCommands());
+    $this->addInteractivityForOption('cmd', self::INTERACTION_UNSET_ONLY, $question);
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     $output->writeln(
-      $input->getOption('intopt')
+      $input->getOption('cmd')
     );
 
     return InteractiveOptionCommand::SUCCESS;
   }
 
+  protected function cmdInputValidator($cmd)
+  {
+    if(!$this->isValidSubCommand($cmd)) {
+      throw new \RuntimeException('\'' . $cmd . '\' is not in the list of valid commands.');
+    }
+    return $cmd;
+  }
+
   protected function validSubCommands()
   {
     return [
-      'output_gpx'
+      'output_gpx',
+      'test'
     ];
   }
 
