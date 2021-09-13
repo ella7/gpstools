4e715f31e26c4b319b4becf107ef24924ba63f61
Ryan Packer <ryan.packer@gmail.com>
2021-09-12 19:28:42 -0600

    additional porting of GPSTrackFactory::buildFromFITFile
    still much cleanup required. brought over as is.

--- src/Service/GPSTrackFactory.php
+++ src/Service/GPSTrackFactory.php
@@ -5,6 +5,8 @@ namespace App\Service;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Cache\CacheInterface;
 use App\Service\FITParser;
+use App\Model\GPSTrack;
+use App\Model\GPSTrackPoint;
 
 class GPSTrackFactory
 {
@@ -77,9 +79,52 @@ class GPSTrackFactory
   public function buildTrackFromFITFile($path)
   {
     $this->fit_parser->setFitPath($path);
-    // $fit_parser = new FITParser($path);
-    // $track = new GPSTrack;
-    // $session_data = $fit_parser->activitySessionData();
+    $track = new GPSTrack();
+    $session_data = $this->fit_parser->activitySessionData();
+
+    // TODO: clean this up to assign dynamically - create $_properties structure
+    // TODO: figure out why dates are off on fit files
+
+    // TODO: should consider just adding track points and letting that calculate track summary info
+    if(array_key_exists('session.start_time', $session_data))             $track->start_time         = $session_data['session.start_time'] + (20*365.25*24*60*60)-(24*60*60);
+    if(array_key_exists('session.num_laps', $session_data))               $track->num_laps           = $session_data['session.num_laps'];
+    if(array_key_exists('session.sport', $session_data))                  $track->sport              = $this->fitEnumData('sport', $session_data['session.sport']);
+    if(array_key_exists('session.total_timer_time[s]', $session_data))    $track->total_timer_time   = $session_data['session.total_timer_time[s]'];
+    if(array_key_exists('session.total_elapsed_time[s]', $session_data))  $track->total_elapsed_time = $session_data['session.total_elapsed_time[s]'];
+    if(array_key_exists('session.total_distance[m]', $session_data))      $track->total_distance     = $session_data['session.total_distance[m]'];
+    if(array_key_exists('session.total_calories[kcal]', $session_data))   $track->total_calories     = $session_data['session.total_calories[kcal]'];
+    if(array_key_exists('session.total_ascent[m]', $session_data))        $track->total_ascent       = $session_data['session.total_ascent[m]'];
+    if(array_key_exists('session.total_descent[m]', $session_data))       $track->total_descent      = $session_data['session.total_descent[m]'];
+    if(array_key_exists('session.avg_speed[m/s]', $session_data))         $track->average_speed      = $session_data['session.avg_speed[m/s]'];
+    if(array_key_exists('session.max_speed[m/s]', $session_data))         $track->max_speed          = $session_data['session.max_speed[m/s]'];
+    if(array_key_exists('session.avg_heart_rate[bpm]', $session_data))    $track->average_heart_rate = $session_data['session.avg_heart_rate[bpm]'];
+    if(array_key_exists('session.max_heart_rate[bpm]', $session_data))    $track->max_heart_rate     = $session_data['session.max_heart_rate[bpm]'];
+
+    $records = $this->fit_parser->activityRecordsData();
+    $max_cadence = 0;
+    $cadence_sum = 0;
+    $cadence_count = 0;
+    foreach($records as $record){
+
+      if(!$track->start_time) {
+        $track->start_time = $record['record.timestamp[s]'];
+      }
+
+      $track_point = new GPSTrackPoint();
+      $track_point->initFromFITArray($record, $track->start_time);
+      $track->addTrackPoint($track_point);
+
+      if(array_key_exists('record.cadence[rpm]', $record)){
+        $cadence = (int)$record['record.cadence[rpm]'];
+        $cadence_sum += $cadence;
+        $cadence_count++;
+        if($cadence > $max_cadence){ $max_cadence = $cadence; }
+      }
+    }
+    if($cadence_sum > 0) $track->average_cadence  = round($cadence_sum/$cadence_count);
+    $track->max_cadence      = $max_cadence;
+
+    return $track;
   }
 
   public static function cacheKeyFromFilePath($path)
@@ -109,5 +154,35 @@ class GPSTrackFactory
       'csv' => 'unicsv',
       'gpx' => 'gpx'
     ];
+  }
+
+  public function fitEnumData($type, $value)
+  {
+    $enum_data = [
+      'sport' => [  // Have capitalised and replaced underscores with spaces.
+        0 => 'Generic',
+        1 => 'Running',
+        2 => 'Cycling',
+        3 => 'Transition',
+        4 => 'Fitness equipment',
+        5 => 'Swimming',
+        6 => 'Basketball',
+        7 => 'Soccer',
+        8 => 'Tennis',
+        9 => 'American football',
+        10 => 'Training',
+        11 => 'Walking',
+        12 => 'Cross country skiing',
+        13 => 'Alpine skiing',
+        14 => 'Snowboarding',
+        15 => 'Rowing',
+        16 => 'Mountaineering',
+        17 => 'Hiking',
+        18 => 'Multisport',
+        19 => 'Paddling',
+        254 => 'All'
+      ]
+    ];
+    return isset($enum_data[$type][$value]) ? $enum_data[$type][$value] : 'unknown';
   }
 }
