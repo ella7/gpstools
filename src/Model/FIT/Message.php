ac6bc72ffafc236c073d01edcbf38182f0e466ba
Ryan Packer <ryan.packer@gmail.com>
2021-09-26 23:56:58 -0400

    Update Message.php
    added setPropertiesFromArray functinoality. seems like this could be a mix-in or something like that - fairly common use case.

--- src/Model/FIT/Message.php
+++ src/Model/FIT/Message.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\FIT;
 
+use function Symfony\Component\String\u;
 
 class Message
 {
@@ -12,5 +13,38 @@ class Message
   protected $type;
   protected $local_number;
   protected $message;
+  protected $fields;
+
+  /**
+   * Attempt to set object properties from an associative array
+   *
+   * @param array $a  An associatve array with keys matching the object vars for `$this`
+   */
+  public function setPropertiesFromArray($a)
+  {
+    $settable_properties = $this->getAutoSettableProperties();
+
+    foreach ($settable_properties as $property_name) {
+      if(array_key_exists($property_name, $a)){
+        // First look for $this->setProperty(), and then use "$this->property = ... "
+        $property_setter = 'set' . u($property_name)->camel()->title();
+        if(method_exists($this, $property_setter)){
+          $this->{$property_setter}($a[$property_name]);
+        } else {
+          $this->{$property_name} = $a[$property_name];
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns the list of properties that can be set by setPropertiesFromArray
+   *
+   * Can be overwritten by children in order to provide more specific control
+   */
+  protected function getAutoSettableProperties()
+  {
+    return array_keys(get_object_vars($this));
+  }
 
 }
