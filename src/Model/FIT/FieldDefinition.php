b75f1d501f0a18f4854999f5355107ae215e432b
Ryan Packer <ryan.packer@gmail.com>
2021-09-28 16:16:47 -0600

    Update FieldDefinition.php
    added functionality around getting and setting units. This is likely going to change to accommodate subfields and ref_fields.

--- src/Model/FIT/FieldDefinition.php
+++ src/Model/FIT/FieldDefinition.php
@@ -2,12 +2,11 @@
 
 namespace App\Model\FIT;
 
-
 class FieldDefinition
 {
 
   protected $name;
-  protected $value; // unsure if this should be called value or perhaps base_type - needs more reasearch
+  protected $value; // TODO: I don't know what value represents in the CSV file
   protected $units;
 
   public function __construct($name, $value, $units)
@@ -15,6 +14,26 @@ class FieldDefinition
     $this->name   = $name;
     $this->value  = $value;
     $this->units  = $units;
+  }
+
+  public function getUnits()
+  {
+    return $this->units;
+  }
+
+  public function setUnitsFromGlobalProfile($message)
+  {
+    $this->units = GlobalProfile::getUnitsForMessageAndFieldType($message, $this->name);
+  }
+
+  // TODO: This seems bad. Need to look at if/how/why this is needed and make sure it belongs. Likely temporary. 
+  public function exportAsArray()
+  {
+    return [
+      'name' => $this->name,
+      'value' => $this->value,
+      'units' => $this->units,
+    ];
   }
 
 }
