4ad6e7f36f6d15799b5e74dfb82837ae593efccb
Ryan Packer <ryan.packer@gmail.com>
2021-10-06 19:23:40 -0600

    Added tests to check for unique component names and no nested subfields
    Currently dependent on two conditions that are not necessarily guaranteed - these tests check the profile to make sure both conditions are met. See issue #3 for more info.

--- tests/Model/FIT/GlobalProfileTest.php
+++ tests/Model/FIT/GlobalProfileTest.php
@@ -11,4 +11,76 @@ final class GlobalProfileTest extends TestCase
     $this->assertIsObject($definition);
     $this->assertEquals('data', $definition->getName());
   }
+
+  /**
+   * The functionality of FIT\Message::getFinalFieldDefinition depends on component names being
+   * unique per message type. This test examines each message type to see if any component names
+   * are repeated.
+   *
+   * This test can be simplified significantly once the GlobalProfile::MESSAGE_TYPES structure
+   * contains FIT\FieldDefintions so that methods of that class can be used rather than manually
+   * traversing the structure.
+   */
+  public function testComponentNamesAreUniquePerMessageType(): void
+  {
+    foreach(GlobalProfile::MESSAGE_TYPES as $message_type){
+
+      $component_names  = [];
+
+      foreach($message_type['fields'] as $field){
+
+        if(array_key_exists('components', $field)){
+          foreach($field['components'] as $component){
+            $message = "Checking message type: "
+              . $message_type['name']
+              . " | field: " . $field['name']
+              . " | component " . $component['name']
+            ;
+            $this->assertNotContains($component['name'], $component_names, $message);
+            $component_names[] = $component['name'];
+          }
+        }
+
+        if(array_key_exists('subfields', $field)){
+          foreach($field['subfields'] as $subfield){
+            if(array_key_exists('components', $subfield)){
+              foreach($subfield['components'] as $component){
+                $message = "Checking message type: "
+                  . $message_type['name']
+                  . " | field: " . $field['name']
+                  . " | subfield: " . $subfield['name']
+                  . " | component " . $component['name']
+                ;
+                $this->assertNotContains($component['name'], $component_names, $message);
+                $component_names[] = $component['name'];
+              } // end foreach component
+            } // end if(subfield has components)
+          } // end foreach subfield
+        } // end if(field has subfields)
+      } // end foreach $field
+    } // end foreach $message_type
+  } // end function testComponentNamesAreUniquePerMessageType()
+
+
+  /**
+   * The App assumes no nested subfields. This test examines each message type to see if any
+   * of the fields contain more than one layer of subfields.
+   *
+   * This test can be simplified significantly once the GlobalProfile::MESSAGE_TYPES structure
+   * contains FIT\FieldDefintions so that methods of that class can be used rather than manually
+   * traversing the structure.
+   */
+  public function testMessageTypesDoNotContainNestedSubfields(): void
+  {
+    foreach(GlobalProfile::MESSAGE_TYPES as $message_type){
+      foreach($message_type['fields'] as $field){
+
+        if(array_key_exists('subfields', $field)){
+          foreach($field['subfields'] as $subfield){
+            $this->assertArrayNotHasKey('subfield', $subfield);
+          } // end foreach subfield
+        } // end if(field has subfields)
+      } // end foreach $field
+    } // end foreach $message_type
+  } // end function testMessageTypesDoNotContainNestedSubfields()
 }
