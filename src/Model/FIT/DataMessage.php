f3f88a2863d8d82842e1b6ce0de92450eb4df247
Ryan Packer <ryan.packer@gmail.com>
2021-10-08 13:34:51 -0600

    Update DataMessage.php
    More fully adopting setPropertiesFromArray() in constructor plus typo and minor change

--- src/Model/FIT/DataMessage.php
+++ src/Model/FIT/DataMessage.php
@@ -7,10 +7,10 @@ class DataMessage extends Message
 
   protected $definition;
 
-  public function __construct(DefinitionMessage $definition)
+  public function __construct($properties)
   {
+    parent::__construct($properties);
     $this->type = Message::MESSAGE_TYPE_DATA;
-    $this->definition = $definition;
   }
 
   /**
@@ -22,6 +22,11 @@ class DataMessage extends Message
   public function getFieldValue(string $field_name)
   {
     return $this->fields[$field_name];
+  }
+
+  public function setDefinition(DefinitionMessage $definition)
+  {
+      $this->definition = $definition;
   }
 
   /**
@@ -39,7 +44,7 @@ class DataMessage extends Message
    * Returns the final FieldDefinition for a given index, name, and this DataMessage
    *
    * FieldDefinitions may contain SubfieldDefinitions and/or ComponentDefinitions whose properties
-   * should supersede those from the FieldDefinition. This method gets the acutal FieldDefintion
+   * should supersede those from the FieldDefinition. This method gets the acutal FieldDefinition
    * that should be used for a given field index and name.
    */
   public function getFinalFieldDefinition(int $field_index, string $field_name)
