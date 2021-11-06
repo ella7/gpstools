fa404a41eedf9f1da2ad63b0950f877177bd18b5
Ryan Packer <ryan.packer@gmail.com>
2021-11-05 21:29:01 -0400

    added FIT\File::getMessage(int $message_index)

--- src/Model/FIT/File.php
+++ src/Model/FIT/File.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Model\FIT;
-
+use App\Model\FIT\Message;
 use App\Utility\AutoSettablePropertiesTrait;
 
 class File
@@ -19,5 +19,15 @@ class File
   public function getMessages()
   {
     return $this->messages;
+  }
+
+  /**
+   * Returns the message at the $message_index position from the FIT file
+   * @param  int    $message_index
+   * @return Message
+   */
+  public function getMessage(int $message_index): Message
+  {
+    return $this->messages[$message_index];
   }
 }
